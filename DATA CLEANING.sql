66406567473133e4d73b6bbaf89a07c1ac86842e
khalid21456 <khalidedaoudi8@gmail.com>
2024-11-02 21:38:00 +0000

    remove duplicates 2

--- DATA CLEANING.sql
+++ DATA CLEANING.sql
@@ -14,3 +14,25 @@ FROM layoffs;
 
 SELECT *
 FROM layoffs_staging;
+
+
+WITH duplicate_cte AS (
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company,location, industry, total_laid_off, percentage_laid_off, `date`,stage ,
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT * 
+FROM duplicate_cte
+WHERE row_num > 1;
+
+SELECT * 
+FROM layoffs_staging
+WHERE company = 'Casper';
+
+-- You can't Update or Delete in a CTE !
+
+
+
+
